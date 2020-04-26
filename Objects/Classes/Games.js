6e75cb3153240048e94c554e719701f1e906e05a
Andreea <andreea79@gmail.com>
2020-04-26 09:13:14 +0300

    button fix

--- Objects/Classes/Games.js
+++ Objects/Classes/Games.js
@@ -7,17 +7,43 @@ function Game(id, title, imageUrl, description){
 }
 
 //adding on the game prototype a function that gives the DOM representation of the game object
+//it includes the functions that add events on DELETE and EDIT buttons
 Game.prototype.displayGame =   function() {
     
     const gameDiv = document.createElement('div');
     
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.innerHTML="Delete Game";
+    
+    const editBtn = document.createElement('button');
+    editBtn.classList.add("editBtn");
+    editBtn.innerHTML="Edit Game";
+    
     gameDiv.innerHTML = `<h1>${this.title}</h1>
                         <img src="${this.imageUrl}"/>
-                        <p>${this.description}</p>
-                        <button class="deleteBtn" id="delete${this._id}">Delete Game</button>
-                        <button class = "editBtn" id="edit${this._id}" >Edit Game</button>`;
+                        <p>${this.description}</p>`
     
     gameDiv.setAttribute("id",`${this._id}`);
-    
+    gameDiv.appendChild(deleteBtn);
+    gameDiv.appendChild(editBtn);
+
+    //adding events on the div's buttons
+    //1.DELETE
+    deleteBtn.addEventListener('click', async function(eventClickDelete){
+        //removing game from DOM
+        gameDiv.remove();
+        //deleting on server
+        await fetchApi.deleteGameOnServer(eventClickDelete.target.parentElement.getAttribute("id"));
+    });
+
+    //2.EDIT
+    editBtn.addEventListener('click', function(eventClickEdit){
+        //creating the editForm and adding it to the game to edit div
+        const gameToEditDiv = eventClickEdit.target.parentElement;
+        const editForm = createEditForm(gameToEditDiv);
+        gameToEditDiv.appendChild(editForm);
+        
+    });
     return gameDiv;
 }
--- Objects/app.js
+++ Objects/app.js
@@ -1,6 +1,6 @@
 const fetchApi = new FetchApi("https://games-app-siit.herokuapp.com");
 
-
+// fct to render the (list of) games in DOM
 async function displayAllGames(){
     //getting games from server
     const gamesFromServer = await fetchApi.getGamesList();
@@ -16,57 +16,13 @@ async function displayAllGames(){
         );
         gamesList.push(game);
     }
-    //iterating through games list array and:
+    //iterating through games list array and
     for(let i=0; i < gamesList.length; i++){
         //rendering each game and adding it to the DOM
         const gameNode = gamesList[i].displayGame();
         
         const container = document.querySelector('.container');
         container.appendChild(gameNode);
-        
-        //adding functionality on buttons:
-        //1.DELETE
-        document.getElementById(`delete${gameNode.id}`).addEventListener('click', async function(eventClickDelete){
-            //selecting the game to delete in DOM
-            const gameDiv = eventClickDelete.target.parentElement;
-            //removing game from DOM
-            gameDiv.remove();
-            //deleting on server
-            await fetchApi.deleteGameOnServer(eventClickDelete.target.parentElement.getAttribute("id"));
-        });
-
-        //2.EDIT
-        document.getElementById(`edit${gameNode.id}`).addEventListener('click', function(eventClickEdit){
-            //selecting the game to edit - HTML obj
-            const gameToEditDiv = eventClickEdit.target.parentElement;
-            
-            //creating the updateForm, adding stye, populating imput fields with data from gameToEditDiv
-            const updateForm = document.createElement('form');
-            updateForm.classList.add('updateForm');
-            updateForm.innerHTML = createEditForm(gameToEditDiv);
-           
-            //appending it to the selected game div
-            eventClickEdit.target.parentElement.appendChild(updateForm);
-            
-            //3 SAVE CHANGES in updateForm
-            document.getElementById(`update${gameToEditDiv.id}`).addEventListener('click', async function(eventClickSaveChanges){
-                
-                eventClickSaveChanges.preventDefault();
-
-                const updatedGame = encodeUpdateFormInput();
-                const apiResponse = await fetchApi.requestGameUpdate(eventClickSaveChanges.target.getAttribute("id"), updatedGame);
-                
-                const gameDivContainer = eventClickSaveChanges.target.parentElement.parentElement.parentElement;
-                    
-                gameDivContainer.querySelector('h1').innerHTML = apiResponse.title;
-                gameDivContainer.querySelector('img').setAttribute("src", apiResponse.imageUrl);
-                gameDivContainer.querySelector("p").innerHTML = apiResponse.description;
-    
-                const formElement = eventClickSaveChanges.target.parentElement.parentElement;
-                
-                formElement.remove();
-            });
-        });
     }
 }
 displayAllGames();
@@ -114,6 +70,129 @@ document.querySelector('.submit-btn').addEventListener('click', function(event) 
    }
 });
 
+//Validate form helper functions
+function validateFormTextInput(input, errorMsg){
+    //console.log(document.querySelector(`[rel="${input.id}]"`));
+    if(input.value === "") {
+        if(!document.querySelector('[rel="' +input.id + '"]')){
+            displayErrorMsg(input, errorMsg);
+        }
+    }else{
+        if(document.querySelector('[rel="' +input.id + '"]')){
+            document.querySelector('[rel="' +input.id + '"]').remove();
+            input.classList.remove('inputError');
+            console.log('the error is erased'); 
+        }
+    }
+}
+
+function validateFormDateInput(input, errMsg){
+    if(isNaN(input.value) && input.value !== ""){
+        displayErrorMsg(input, errMsg);
+    }
+}
+
+function displayErrorMsg(element, msg) {
+    element.classList.add ('inputError');
+    const errorMessage = document.createElement('span');
+    errorMessage.setAttribute("rel", element.id); 
+    errorMessage.classList.add('errorMsg');
+    errorMessage.innerText = msg;
+    element.after(errorMessage);
+}
+
+//fct for rendering new created game in Dom 
+async function displayNewGame(game){
+
+    const newGameResponse = await fetchApi.createNewGameRequest(game);
+    const newGame = new Game(newGameResponse._id, newGameResponse.title, newGameResponse.imageUrl, newGameResponse.description);
+    
+    const newGameNode = newGame.displayGame();
+   // console.log(newGameNode);
+    document.querySelector('.container').appendChild(newGameNode);
+}
+
+//fct for deleting game from dom
+function deleteDomElement(element){
+    element.remove();
+}
+
+//fct to create the edit form, based on the game (HTMLobject) to edit
+//it includes the fct that adds event on SAVE CHANGES (update) button
+function createEditForm(gameToEdit){
+    //collecting data from the game to edit HTML div
+    const id = gameToEdit.getAttribute("id")
+    const title = gameToEdit.querySelector('h1').innerHTML;
+    const image = gameToEdit.querySelector('img').getAttribute("src");
+    const description = gameToEdit.querySelector('p').innerHTML;
+    
+    //saving the SAVE CHANGES (update) button in a constant, to later add event on it
+    const updateBtn = document.createElement('button');
+    updateBtn.classList.add("update-btn");
+    updateBtn.setAttribute("id", `update${id}`);
+    updateBtn.innerHTML="Save Changes";
+    
+    //creating the editForm,styling it and populating it with data from the game to edit div
+    const editForm = document.createElement('form');
+    editForm.classList.add('updateForm');
+    editForm.innerHTML = `<form class="updateForm" action="" method="POST">
+                    <div class="element-wrapper">
+                    <label for="updateTitle">Edit Title</label>
+                    <input type="text" id="updateTitle" value="${title}"/>
+                    </div>
+                    <div class="element-wrapper">
+                    <label for="updateImageUrl">Edit Image URL</label>
+                    <input type="text" id="updateImageUrl" value="${image}"/>
+                    </div>
+                    <div class="element-wrapper">
+                    <label for="updateDescription">Edit Description</label>
+                    <textarea id="updateDescription" rows="5">${description}</textarea>
+                    </div>
+                    <div class="buttons-wrapper">
+                    <button class="cancel-btn">Cancel</button>
+                    </div>
+                    </form>`;
+    //appending the button to the editForm
+    editForm.lastElementChild.appendChild(updateBtn);
+    
+    //adding event on SAVE CHANGES button
+        updateBtn.addEventListener('click', async function(eventClickSaveChanges){
+            
+            eventClickSaveChanges.preventDefault();
+            //sending the encoded editForm input data to the server and saving the response
+            const updatedGame = encodeUpdateFormInput();
+            const apiResponse = await fetchApi.requestGameUpdate(eventClickSaveChanges.target.getAttribute("id"), updatedGame);
+            
+            //updating the selected game to edit div, based on the server response
+            const gameDivContainer = eventClickSaveChanges.target.parentElement.parentElement.parentElement;
+                
+            gameDivContainer.querySelector('h1').innerHTML = apiResponse.title;
+            gameDivContainer.querySelector('img').setAttribute("src", apiResponse.imageUrl);
+            gameDivContainer.querySelector("p").innerHTML = apiResponse.description;
+
+            const formElement = eventClickSaveChanges.target.parentElement.parentElement;
+            //removing the form from DOM
+            formElement.remove();
+        });
+    
+    return editForm;
+}
+//function to colect and encode updated data from editForm
+function encodeUpdateFormInput(){
+    //1 collecting data from the updateForm
+    const title = document.getElementById('updateTitle');
+    const description = document.getElementById('updateDescription');
+    const imageUrl = document.getElementById('updateImageUrl');
+
+    //2 encoding it to send to server
+    const urlencoded = new URLSearchParams();
+    
+    urlencoded.append("title", title.value);
+    urlencoded.append("description", description.value);
+    urlencoded.append("imageUrl", imageUrl.value);
+    
+    return urlencoded;
+}
 
 
 
